1c016e9852b9874ca1a1a8e3be888e19c368957a
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-03-14 15:23:01 -0400

    Mar-14-2024-3-23-00-PM

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+
+<script setup>
+  import ImageGallery from './components/ImageGallery.vue';
+  import SideBar from './components/SideBar.vue'
+  import { ref } from 'vue';
+
+  const isWithColor = ref(true);
+
+  const onSelectOption = (isColor) => {
+    isWithColor.value = isColor;
+  } 
+</script>
+
+<template>
+  <v-app>
+    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    {{ isWithColor }}
+    yyyy
+    <SideBar @selectOption="onSelectOption" />
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor"/>
+    </v-main>
+  </v-app>  
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import { defineEmits } from 'vue'; 
+
+  const emits = defineEmits(['selectOption']);
+
+  const selectOption = (isColor) => {
+    emits('selectOption', isColor);
+  }
+</script>
+
+<template>
+      <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/86.jpg" title="Jonathan" nav>
+          <template v-slot:append>
+            <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item 
+            prepend-icon="mdi-image" 
+            title="Color Gallery" 
+            value="color"
+            @click="selectOption(true)"
+            >
+            
+          </v-list-item>
+          <v-list-item prepend-icon="mdi-dialpad" title="Gray Gallery" 
+            value="gray"
+            @click="selectOption(false)"
+          >
+
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 220px"></v-main>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      rail: true,
+    }
+  },
+}
+</script>
